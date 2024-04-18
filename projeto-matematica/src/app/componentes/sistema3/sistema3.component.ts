87525c48a7330ed7267e870b144a37f26f73e2d7
David <davidmaraospungin@gmail.com>
2024-04-18 18:28:47 -0300

    commitei

--- projeto-matematica/src/app/componentes/sistema3/sistema3.component.ts
+++ projeto-matematica/src/app/componentes/sistema3/sistema3.component.ts
@@ -9,18 +9,50 @@ import { Component } from '@angular/core';
 export class Sistema3Component {
   numero1: number = 0;
   numero2: number = 0 ;
-  resultado: number = 0;
-  ordemx: number = 0;
-  ordemy: number = 0;
+  resultado: number[][] = [];
+  mostrarEscalar: boolean = false; // variável para controlar a visibilidade
+  ordemx: number = 3;
+  ordemy: number = 4;
+  matrizTeste: number[][] =[[1,-1,1,1],
+                            [2,1,2,0],
+                            [3,-1,1,1]];
+  matrizEcalonada :boolean =false;
+  
 
-  gerarMatriz()
+  
+
+  MetodoDeGauss()
   {
-    for (let n=0)
-  }
+    for(let i = 0;i<this.ordemx;i++)
+    {
+        for(let j=0;j<this.ordemy;j++)
+        {
+            if(i>j){
+              if(this.matrizTeste[i][j]== 0)
+              {
+                this.matrizEcalonada = true;
+              }
+              else
+              {
+                let aux1= this.matrizTeste[i][j];
+                let aux2= this.matrizTeste[i-1][j];
+                let divisor = -1*(aux1/aux2);
+                for(let y =0 ;y < this.ordemy;y++)
+                {
+                  this.matrizTeste[i][y] = this.matrizTeste[i][y] + (this.matrizTeste[i-1][y]/divisor);
+                }
 
+              }
+            }
+        }
+    }
+    
+
+  }                          
 
   somar() {
-    this.resultado = this.numero1 + this.numero2;
+    //this.resultado = this.numero1 + this.numero2;
+    
   }
 
 }
