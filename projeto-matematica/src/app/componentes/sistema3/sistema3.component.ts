92a24b9775ba9a46992be8799d2d30d5ca3bce27
David <davidmaraospungin@gmail.com>
2024-04-21 22:45:29 -0300

    Falta html e tipo de solução

--- projeto-matematica/src/app/componentes/sistema3/sistema3.component.ts
+++ projeto-matematica/src/app/componentes/sistema3/sistema3.component.ts
@@ -12,11 +12,25 @@ export class Sistema3Component {
   resultado: number[][] = [];
   mostrarEscalar: boolean = false; // variável para controlar a visibilidade
   ordemx: number = 3;
-  ordemy: number = 4;
+  ordemy: number = this.ordemx+1;
   matrizTeste: number[][] =[[1,-1,1,1],
                             [2,1,2,0],
                             [3,-1,1,1]];
+  variaveis: number[]=[];
+  subtracoes: number = 0
   matrizEcalonada :boolean =false;
+  multiplicador :number = 0;
+  matriz: number[][] = [[]];
+  GerarMatrix() {
+    this.matriz = [];
+    for (let i = 0; i < this.numero1; i++) {
+      this.matriz.push([]);
+      for (let j = 0; j < this.numero2; j++) {
+        this.matriz[i].push(0);
+      }
+    }
+    this.mostrarEscalar = true; // torna visível após gerar a matriz
+  }
   
 
   
@@ -35,24 +49,63 @@ export class Sistema3Component {
               else
               {
                 let aux1= this.matrizTeste[i][j];
-                let aux2= this.matrizTeste[i-1][j];
+                let aux2= this.matrizTeste[j][j];
                 let divisor = -1*(aux1/aux2);
-                for(let y =0 ;y < this.ordemy;y++)
-                {
-                  this.matrizTeste[i][y] = this.matrizTeste[i][y] + (this.matrizTeste[i-1][y]/divisor);
-                }
+                
+                    for(let y =0 ;y < this.ordemy;y++)
+                    {
+                      let sub=   (this.matrizTeste[j][y]*divisor);
+                      this.matrizTeste[i][y] += sub;
+                      //console.log(sub,"=",this.matrizTeste[j][y],"*",divisor);
+                    }
+                  
+                //console.log("mudança");
+                //console.log(aux1," ",aux2," ",divisor," ", i," ",j);
 
               }
             }
         }
     }
-    
+    for(let i = 0;i<this.ordemx;i++)
+      {
+          for(let j=0;j<this.ordemy;j++)
+          {
+            if(i>j){
+              
+            }
+          }
+      }
 
   }                          
 
-  somar() {
-    //this.resultado = this.numero1 + this.numero2;
-    
+  Calcular() {
+    for(let a = this.ordemx - 1 ;a>=0;a--)
+      {
+        for(let b = this.ordemx - 1; b >= 0;b--)
+          {
+            if(this.matrizTeste[a][b]|| 0){
+              if(a==b){
+                this.multiplicador = this.matrizTeste[a][b];
+                console.log("multiplicador ",this.multiplicador);
+              }
+              else{
+                this.subtracoes += this.matrizTeste[a][b]*this.variaveis[this.ordemx -1 - b];
+              }
+            }
+
+          }
+          
+          let resultado = this.matrizTeste[a][this.ordemy-1]- this.subtracoes;
+          this.variaveis.push(resultado/this.multiplicador);
+          console.log("sutraçoes", this.subtracoes,"resultado ", resultado);
+          
+      }
+
+    for(let v= this.ordemx-1;v>=0;v--)
+      {
+        let nomeVariavel = this.ordemx - v;
+        console.log("X",nomeVariavel," = ",this.variaveis[v]);
+      }
   }
 
 }
